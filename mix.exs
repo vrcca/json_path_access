f745d830c1036453618294de1e06fd7ab77819cc
Vitor Cavalcanti <1920397+vrcca@users.noreply.github.com>
2022-07-18 20:48:56 +0200

    Relaxes Elixir versions based on nimble_parsec

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule JsonPathAccess.MixProject do
     [
       app: :json_path_access,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
