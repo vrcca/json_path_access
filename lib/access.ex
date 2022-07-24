63ba4f938692c4763aaa7cc8f027b2f94173fa80
Vitor Cavalcanti <1920397+vrcca@users.noreply.github.com>
2022-07-24 14:31:52 +0200

    Implements stepped range updates

--- lib/access.ex
+++ lib/access.ex
@@ -7,23 +7,39 @@ defmodule JsonPathAccess.Access do
     fn op, data, next -> slice(op, data, range, next) end
   end
 
-  defp slice(:get, data, range, next) do
+  defp slice(:get, data, %Range{} = range, next) do
     next.(Enum.slice(data, range))
   end
 
-  defp slice(:get_and_update, data, %Range{first: first, last: last} = range, next) do
+  defp slice(:get_and_update, data, range, next) do
     slice = Enum.slice(data, range)
 
     case next.(slice) do
       :pop ->
+        # TODO: respect indexes from range
         {slice, data -- slice}
 
       {get, changes} ->
-        begin = if first >= 0, do: 0, else: length(data)
-        start = Enum.take(data, begin + first)
-        end_list = Enum.drop(data, begin + last + 1)
-        changes = start ++ changes ++ end_list
+        changes = merge(data, changes, range)
+
         {get, changes}
     end
+  end
+
+  def merge(data, changes, range) do
+    begin = if(range.first >= 0, do: 0, else: length(data))
+
+    {_index, acc, _current} =
+      for element <- data, reduce: {0, [], changes} do
+        {index, acc, changes} ->
+          if (index - begin) in range do
+            [head | tail] = changes
+            {index + 1, [head | acc], tail}
+          else
+            {index + 1, [element | acc], changes}
+          end
+      end
+
+    Enum.reverse(acc)
   end
 end
--- test/access_test.exs
+++ test/access_test.exs
@@ -5,43 +5,63 @@ defmodule JsonPathAccess.AccessTest do
 
   describe "slice/1" do
     test "retrieves a range from the start of the list" do
-      list = ["a", "b", "c", "d", "e", "f", "g"]
-      assert ["b", "c"] == get_in(list, [Access.slice(1..2)])
+      list = ~w(a b c d e f g)
+      assert ~w(b c) == get_in(list, [Access.slice(1..2)])
     end
 
     test "retrieves a range from the end of the list" do
-      list = ["a", "b", "c", "d", "e", "f", "g"]
-      assert ["f", "g"] == get_in(list, [Access.slice(-2..-1)])
+      list = ~w(a b c d e f g)
+      assert ~w(f g) == get_in(list, [Access.slice(-2..-1)])
+    end
+
+    test "retrieves a range with steps" do
+      list = ~w(a b c d e f g)
+      assert ~w(a c) == get_in(list, [Access.slice(0..2//2)])
+      assert ~w(b e) == get_in(list, [Access.slice(1..4//3)])
+      assert ~w(b) == get_in(list, [Access.slice(1..2//3)])
+      assert ~w(a c e g) == get_in(list, [Access.slice(0..6//2)])
     end
 
     test "drops a range from the start of the list" do
-      list = ["a", "b", "c", "d", "e", "f", "g"]
-      assert {["b", "c"], ["a", "d", "e", "f", "g"]} == pop_in(list, [Access.slice(1..2)])
+      list = ~w(a b c d e f g)
+      assert {~w(b c), ~w(a d e f g)} == pop_in(list, [Access.slice(1..2)])
     end
 
     test "drops a range from the end of the list" do
-      list = ["a", "b", "c", "d", "e", "f", "g"]
-      assert {["f", "g"], ["a", "b", "c", "d", "e"]} == pop_in(list, [Access.slice(-2..-1)])
+      list = ~w(a b c d e f g)
+      assert {~w(f g), ~w(a b c d e)} == pop_in(list, [Access.slice(-2..-1)])
+    end
+
+    test "drops a range with steps" do
+      list = ~w(a b c d e f g)
+      assert {~w(a c e), ~w(b d f g)} == pop_in(list, [Access.slice(0..4//2)])
+      assert {~w(b), ~w(a c d e f g)} == pop_in(list, [Access.slice(1..2//2)])
     end
 
     test "updates range from the start of the list" do
-      list = ["a", "b", "c", "d", "e", "f", "g"]
-
-      assert ["A", "b", "c", "d", "e", "f", "g"] ==
-               update_in(list, [Access.slice(0..0)], &upcase_items/1)
-
-      assert ["a", "B", "C", "d", "e", "f", "g"] ==
-               update_in(list, [Access.slice(1..2)], &upcase_items/1)
+      list = ~w(a b c d e f g)
+      assert ~w(A b c d e f g) == update_in(list, [Access.slice(0..0)], &upcase_items/1)
+      assert ~w(a B C d e f g) == update_in(list, [Access.slice(1..2)], &upcase_items/1)
     end
 
     test "updates range from the end of the list" do
-      list = ["a", "b", "c", "d", "e", "f", "g"]
+      list = ~w(a b c d e f g)
+      assert ~w(a b c d e F G) == update_in(list, [Access.slice(-2..-1)], &upcase_items/1)
+      assert ~w(A B c d e f g) == update_in(list, [Access.slice(-7..-6)], &upcase_items/1)
+    end
 
-      assert ["a", "b", "c", "d", "e", "F", "G"] ==
-               update_in(list, [Access.slice(-2..-1)], &upcase_items/1)
+    test "updates  a range with steps" do
+      list = ~w(a b c d e f g)
+      assert ~w(A b C d E f g) == update_in(list, [Access.slice(0..4//2)], &upcase_items/1)
+    end
+  end
 
-      assert ["A", "B", "c", "d", "e", "f", "g"] ==
-               update_in(list, [Access.slice(-7..-6)], &upcase_items/1)
+  describe "merge/3" do
+    test "merge list into another using range" do
+      list = ~w(a b c)
+      another_list = ~w(B)
+      range = 1..2//2
+      assert ~w(a B c) == Access.merge(list, another_list, range)
     end
   end
 
