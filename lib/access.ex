ab7f2edf9ba5abf20b5779c7947fe483a2d0165c
Vitor Cavalcanti <1920397+vrcca@users.noreply.github.com>
2024-05-12 18:56:10 +0200

    Style for combine fun

--- lib/access.ex
+++ lib/access.ex
@@ -2,28 +2,26 @@ defmodule JsonPathAccess.Access do
   @moduledoc false
 
   def combine(funs) do
-    fn op, data, next -> combine(op, data, funs, next) end
+    fn op, data, next_fun -> combine(op, data, funs, next_fun) end
   end
 
-  defp combine(:get, data, funs, next) do
-    Enum.map(funs, fn fun ->
-      fun.(:get, data, next)
-    end)
-    |> next.()
+  defp combine(:get, data, funs, next_fun) do
+    result = Enum.map(funs, fn fun -> fun.(:get, data, next_fun) end)
+    next_fun.(result)
   end
 
-  defp combine(:get_and_update, data, funs, next) do
-    pop = next.(hd(data)) == :pop
-    next = if pop, do: &{&1, :pop}, else: next
+  defp combine(:get_and_update, data, funs, next_fun) do
+    pop = next_fun.(hd(data)) == :pop
+    next_fun = if pop, do: fn data -> {data, :pop} end, else: next_fun
 
     {gets, update} =
       Enum.reduce(funs, {[], data}, fn fun, {gets, update} ->
-        {new_gets, new_update} = fun.(:get_and_update, update, next)
+        {new_gets, new_update} = fun.(:get_and_update, update, next_fun)
         {[new_gets | gets], new_update}
       end)
 
     update = if pop, do: Enum.reject(update, &(&1 == :pop)), else: update
 
-    {:lists.reverse(gets), update}
+    {Enum.reverse(gets), update}
   end
 end
